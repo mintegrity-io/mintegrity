c52cba6703bfed882f99d29ac1b8cc7b74dd3c0d
Valerii Zhyla <orator9x@gmail.com>
2025-05-13 09:25:16 +0200

    Finished basic transaction graph modeling

--- scripts/commons/model.py
+++ scripts/commons/model.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
-from enum import StrEnum
+from enum import StrEnum, IntEnum
 
-from scripts.commons import checks
+from scripts.commons.logging_config import get_logger
+
+log = get_logger()
 
 
 class InteractionDirection(StrEnum):
@@ -9,17 +11,38 @@ class InteractionDirection(StrEnum):
     OUTGOING = "outgoing"
 
 
+class AddressType(IntEnum):
+    WALLET = 0
+    CONTRACT = 1
+
+
 @dataclass(frozen=True)
 class Address:
     address: str
+    type: AddressType
 
     def __post_init__(self):
-        checks.check_ethereum_address_validity(self.address)
+        self.check_ethereum_address_validity()
+
+    def check_ethereum_address_validity(self):
+        """
+        Validates if the given string is a valid Ethereum address.
+
+        :return: True if valid, False otherwise
+        """
+        if not self.address.startswith("0x") or len(self.address) != 42:
+            raise ValueError(f"Invalid Ethereum address: {self.address}")
+        else:
+            log.debug(f"Valid Ethereum address: {self.address}")
 
 
 @dataclass(frozen=True)
 class SmartContract:
     address: Address
+
+    def __post_init__(self):
+        if not self.address.type == AddressType.CONTRACT:
+            raise ValueError(f"Address {self.address} is not a contract address. Type: {self.address.type.name}")
 
 
 @dataclass(frozen=True)
--- scripts/graph/transactions_graph.py
+++ scripts/graph/transactions_graph.py
@@ -8,53 +8,95 @@ log = get_logger()
 
 
 class NodeType(IntEnum):
-    # TODO I want to store the type of address in the address object and not in the node
-    WALLET = 0
-    CONTRACT = 1
+    ROOT = 1
+    WALLET = 2
+    CONTRACT = 3
 
 
-@dataclass(frozen=True)
+@dataclass
 class Node:
     address: Address
     type: NodeType
 
 
-@dataclass(frozen=True)
+@dataclass
 class Edge:
     from_node: Node
     to_node: Node
-    transaction: Transaction
+    transactions: set[Transaction]  # it is possible and likely, that multiple transactions exist between the same nodes, e.g. many interactions between two wallets
 
+    def add_transaction(self, transaction: Transaction):
+        if transaction not in self.transactions:
+            self.transactions.add(transaction)
+            log.info(f"Transaction added to edge: {transaction.transaction_hash} from {self.from_node.address} to {self.to_node.address}")
+        else:
+            log.debug(f"Transaction {transaction.transaction_hash} already exists in edge. Skipping addition")
+
+    def get_total_transactions_value(self) -> float:
+        return sum(transaction.value for transaction in self.transactions)
 
 
 class TransactionsGraph:
     nodes: set[Node] = {}
     edges: set[Edge] = {}
 
-    def add_node(self, node: Node):
-        if node not in self.nodes:
-            self.nodes.add(node)
-            log.info(f"Node added: {node.address} of type {node.type.name}")
+    def add_node_if_not_exists(self, address: Address, is_root: bool = False) -> Node:
+        if not self.is_node_in_graph_by_address(address):
+            if is_root:
+                node_type = NodeType.ROOT
+            else:
+                node_type = NodeType.WALLET if address.type == AddressType.WALLET else NodeType.CONTRACT
+            new_node = Node(address, node_type)
+            self.nodes.add(new_node)
+            log.info(f"Node added: {new_node.address} of type {new_node.type.name}")
         else:
-            log.debug(f"Node {node.address} already exists. Skipping addition")
+            log.debug(f"Node with address {address} in graph already exists. Skipping addition")
 
-    # TODO REWORK THIS - is no more valid
-    def add_edge(self, transaction: Transaction):
-        if transaction not in self.edges:
-            self.edges.add(transaction)
-            log.info(f"Transaction added: {transaction.from_node.address} -> {transaction.to_node.address} with value {transaction.value}")
+        return self.get_node_by_address(address)
+
+    def add_edge_if_not_exists(self, from_node: Node, to_node: Node) -> Edge:
+        if not self.is_edge_between_nodes_in_graph(from_node, to_node):
+            new_edge = Edge(from_node, to_node, set())
+            self.edges.add(new_edge)
+            log.info(f"Edge added: {from_node.address} -> {to_node.address}")
+        else:
+            log.debug(f"Edge between {from_node.address} and {to_node.address} already exists. Skipping addition")
+
+        return self.get_edge_by_nodes(from_node, to_node)
 
     def is_transaction_in_graph(self, transaction: Transaction) -> bool:
-        return transaction in self.edges
-
-    def is_transaction_in_graph_by_id(self, tx_hash) -> bool:
-        for transaction in self.edges:
-            if transaction.tx_hash == tx_hash:
+        for edge in self.edges:
+            if transaction in edge.transactions:
                 return True
         return False
 
-    def get_node_by_address(self, address: str) -> Node | None:
+    def is_node_in_graph_by_address(self, address: Address) -> bool:
+        return self.get_node_by_address(address) is not None
+
+    def get_node_by_address(self, address: Address) -> Node | None:
         for node in self.nodes:
             if node.address == address:
                 return node
         return None
+
+    def is_edge_between_nodes_in_graph(self, from_node, to_node) -> bool:
+        return self.get_edge_by_nodes(from_node, to_node) is not None
+
+    def get_edge_by_nodes(self, from_node, to_node) -> Edge | None:
+        for edge in self.edges:
+            if edge.from_node == from_node and edge.to_node == to_node:
+                return edge
+        return None
+
+    def add_transaction(self, transaction: Transaction):
+        if self.is_transaction_in_graph(transaction):
+            log.debug(f"Transaction {transaction.transaction_hash} already exists in graph. Skipping addition")
+            return
+
+        from_node: Node = self.add_node_if_not_exists(transaction.address_from)
+        to_node: Node = self.add_node_if_not_exists(transaction.address_to)
+
+        edge: Edge = self.add_edge_if_not_exists(from_node, to_node)
+        edge.add_transaction(transaction)
+
+
